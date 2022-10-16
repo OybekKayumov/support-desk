420d7a27b66bc7f66101402cf1160e573e715cd3
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-16 17:06:13 +0500

    Update backend-server.js: postman GET request ststus 200

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server...');
+// console.log('Server...');
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+//TODO: Postman request check
+app.get('/', (req, res) => {
+  // res.send('Hello!')
+  res.json({message: 'Welcome to the Support Desk API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
