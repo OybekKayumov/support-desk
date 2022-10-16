bf828182769c437b19ec15ab0177ef84cad8b2ad
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-16 17:34:16 +0500

    Add controllers/userControllers -> registerUser and loginUser

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
 
-router.post('/', (req, res) => {
-  res.send('Register Route')
-})
+router.post('/', registerUser)
 
 // Login
-router.post('/login', (req, res) => {
-  res.send('Login Route')
-})
+router.post('/login', loginUser)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// console.log('Server...');
-
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
