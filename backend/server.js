7936d14347c622ae1ef69ce2c64b21e020d2e006
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-16 17:09:59 +0500

    Update backend-server.js: postman GET /api/users

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 //TODO: Postman request check
-app.get('/', (req, res) => {
+app.get('/api/users', (req, res) => {
   // res.send('Hello!')
   // res.json({message: 'Welcome to the Support Desk API'})
-  res.status(201).json({message: 'Welcome to the Support Desk API'})
+  res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
