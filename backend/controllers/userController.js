7489093c68534e4823e0197c605ae07991920276
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-17 20:36:45 +0500

    Add asyncHandler to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,35 @@
+const asyncHandler = require('express-async-handler')
+
+// express.get('/', asyncHandler(async (req, res, next) => {
+// 	const bar = await foo.findAll();
+// 	res.send(bar)
+// }))
+
 // @desc    Register a new user
 // @route   /api/user/
 // @access  Public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   // console.log(req.body); POST name, email, pwd: /api/users
 
   const {name, email, password} = req.body
 
   //* Validation
-  if (!name || !email || !password) {
-    // return res.status(400).json({ message: 'Please include all fields'}) 
+  if (!name || !email || !password) {    
     res.status(400)
      throw new Error('Please include all fields') 
-
   }
 
   res.send('Register Route')
-}
+})
 
 // @desc    Login a user
 // @route   /api/user/login
 // @access  Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
-}
+})
 
 module.exports = {
   registerUser,
