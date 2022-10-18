743e27a8c82dd7eacebec18be11705ae00c6941d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-18 06:19:22 +0500

    Add connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
-const PORT = process.env.PORT || 8000
+const connectBD = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+
+//TODO:: CONNECT TO DATABASE
+connectBD()
 
 const app = express()
 
