de9aa3af1751e2b25a7ada8c49557386a1f540d9
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-18 07:17:42 +0500

    Add user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
 
-// express.get('/', asyncHandler(async (req, res, next) => {
-// 	const bar = await foo.findAll();
-// 	res.send(bar)
-// }))
+const User = require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/user/
@@ -20,7 +18,37 @@ const registerUser = asyncHandler(async (req, res) => {
      throw new Error('Please include all fields') 
   }
 
-  res.send('Register Route')
+  // Find is user already exists
+  const userExists = await User.findOne({email})
+
+  if(userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+  
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new error('Invalid user data !!!')
+  }
+
+  // res.send('Register Route')
 })
 
 // @desc    Login a user
