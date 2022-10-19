da9c712fe000745556ae549745024a95147f0be1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-19 07:46:59 +0500

    Add frontend/pages: Home, Login, Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      My App
-    </div>
+    <>
+     <Router>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+
+      </div>
+     </Router> 
+    </>
   );
 }
 
