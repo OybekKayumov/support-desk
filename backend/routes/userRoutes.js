80f09b0197c5c308d2d7509ec1417bc96a98c76e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-19 05:08:07 +0500

    Add protect module to userRoutes/me

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,12 @@ const {
   getMe, 
 } = require('../controllers/userController')
 
+const { protect } = require('../middleware/authMiddleware')
+
 router.post('/', registerUser)
 
 // Login
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 module.exports = router
