124fa4ce14f9e2c29b4413a9baeeba853f7b60c7
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-20 05:40:43 +0500

    Add pages/Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+
 function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <section className="heading">
+        <h2>What do you need help with?</h2>
+        <p>Please choose from options below</p>
+      </section>
+
+      <Link to='/new-ticket' className="btn btn-reverse btn-block">
+
+      </Link>
+    </>
   )
 }
 
