dba0827f53c3bbab9a2a7ae2cd0a78b48c24097b
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-20 05:01:53 +0500

    Add pages/Register: formData

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
+import {FaUser} from "react-icons/fa"
+
+const defaultState = {
+  name: '',
+  email: '',
+  password: '',
+  password2: ''
+};
+
 function Register() {
+  const [formData, setFormData] = useState(defaultState)
+
+  const { name, email, password, password2} = formData
+
+  const onChange = () => {
+    setFormData((prevState) => ({
+      
+    }))
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser/> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input 
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              onChange={onChange}
+              placeholder='Enter your name: ' 
+            />
+          </div>
+        </form>
+      </section>
+      
+    </>
   )
 }
 
