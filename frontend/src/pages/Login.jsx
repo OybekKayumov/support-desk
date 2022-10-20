43e110de5aad5264faa0e078b4ae076daadcb408
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-20 05:35:28 +0500

    Add pages/Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,78 @@
+// import e from "express";
+import { useState } from "react";
+import {toast} from 'react-toastify';
+import {FaSignInAlt} from "react-icons/fa";
+
+// const defaultState = {
+//   email: '',
+//   password: '',
+// };
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+     e.preventDefault();
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt/> Login
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email: '
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <input 
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder='Enter your password: '
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+      
+    </>
   )
 }
 
