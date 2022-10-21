e9194cd56f9c94c6592f141270d176f1f8375485
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-21 05:32:43 +0500

    Add authSlice: extraReducers

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const initialState = {
   user: null,
@@ -35,7 +36,21 @@ export const authSlice = createSlice ({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
   }
 })
 
