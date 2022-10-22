00468010a770db1eca1ccdf25b562e0b95ab4307
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-22 08:02:26 +0500

    Add backend\controllers: ticketController- create ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -25,8 +25,29 @@ const getTickets = asyncHandler(async (req, res) => {
 // @route   POST /api/tickets/
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
+  const { product, description } = req.body
+
+  if(!product || !description) {
+    res.status(400)
+    throw new Error('Please add a product and description')
+  }
+
+  // get user using the ID in th JWT
+  const user = await User.findById(req.user.id)
   
-  res.status(200).json({ message: 'createTicket'})
+  if(!user) {
+    res.status(401)
+    throw new Error('User nor found')    
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new'
+  })
+  
+  res.status(201).json(ticket)
 })
 
 module.exports = {
