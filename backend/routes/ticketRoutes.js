ce21f5912724c8784ae7a547a6a235d303b34bda
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-22 06:47:59 +0500

    Add backend\controllers: ticketController- create ticket

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { getTickets, createTicket } = require('../controllers/ticketController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 // api request
-router.route('/').get(protect, getTickets).post(protect, createTickets)
+router.route('/').get(protect, getTickets).post(protect, createTicket)
 
 module.exports = router
