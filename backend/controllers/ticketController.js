36594e4c82a86737e0a183fc4e264623b610c44e
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-22 06:44:46 +0500

    Add backend\controllers: ticketController- create ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel')
 const Ticket = require('../models/ticketModel')
 
 // @desc    Get user tickets
-// @route   /api/tickets
+// @route   GET /api/tickets
 // @access  Private
 
 const getTickets = asyncHandler(async (req, res) => {
@@ -16,3 +16,16 @@ const getTickets = asyncHandler(async (req, res) => {
 
   res.status(200).json({ message: 'getTickets'})
 })
+
+// @desc    Create a new tickets
+// @route   POST /api/tickets/
+// @access  Private
+const createTicket = asyncHandler(async (req, res) => {
+  
+  res.status(200).json({ message: 'createTicket'})
+})
+
+module.exports = {
+  getTickets,
+  createTicket
+}
