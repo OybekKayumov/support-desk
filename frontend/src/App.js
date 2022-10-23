db3d652ce558b5efb33f75fc75b87233fb93e8df
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-23 07:29:44 +0500

    Add frontend: app.js add PrivateRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -19,7 +20,10 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/new-ticket' element={<NewTicket />} />
+          
+          <Route path='/new-ticket' element={<PrivateRoute />}> 
+            <Route path='/new-ticket' element={<NewTicket />} />
+          </Route>
         </Routes>
 
       </div>
