30c710207771af9c49d293b7c4f882e8e010fd0d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-23 06:55:27 +0500

    Add ticket route

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -20,6 +20,33 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(ticket)
 })
 
+// @desc    Get user tickets
+// @route   GET /api/tickets/:id
+// @access  Private
+
+const getTicket = asyncHandler(async (req, res) => {
+  // get user using the ID in th JWT
+  const user = await User.findById(req.user.id)
+  
+  if(!user) {
+    res.status(401)
+    throw new Error('User not found')    
+  }
+  const ticket = await Ticket.findById( req.params.id)
+
+  if(!ticket) {
+    res.status(404) 
+    throw new Error('Ticket no found...')
+  }
+
+  if(ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized...')
+  }
+ 
+  res.status(200).json(ticket)
+})
+
 // @desc    Create new ticket
 // @route   POST /api/tickets
 // @access  Private
