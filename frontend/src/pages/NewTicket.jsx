d072a1cad804c88bc83e6ceba71d402a70da92e5
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-24 04:58:10 +0500

    Add new ticket 1

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -3,16 +3,36 @@ import {useSelector} from 'react-redux';
 
 function NewTicket() {
   const {user} = useSelector((state) => state.auth);
-  const [name, setName] = useState(user.name);
-  const [email, setEmail] = useState(user.email);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
   const [product, setProduct] = useState('iPhone');
   const [description, setDescription] = useState('');
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+  
   return (
     <>
       <section>
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
+      </section>
+
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className='form-control' value={name} disabled />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor="email">Customer Email</label>
+          <input type="text" className='form-control' value={email} disabled />
+        </div>
+
+        <form onSubmit={onSubmit}>
+
+        </form>
       </section>
     </>
   )
