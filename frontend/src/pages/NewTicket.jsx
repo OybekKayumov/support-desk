0d5323127e3835dccbce66dd8bf7b0db269c537a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-24 04:51:09 +0500

    Add new ticket

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
 
 function NewTicket() {
+  const {user} = useSelector((state) => state.auth);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [product, setProduct] = useState('iPhone');
+  const [description, setDescription] = useState('');
+
   return (
-    <div>NewTicket</div>
+    <>
+      <section>
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+    </>
   )
 }
 
