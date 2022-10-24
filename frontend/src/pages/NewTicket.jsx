ab264ac3457f079918eec744ccbadd3845185104
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-24 08:26:16 +0500

    Update ticketService

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,15 +1,44 @@
-import React, {useState} from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { createTicket, reset } from '../features/tickets/ticketSlice';
+import Spinner from '../components/Spinner';
+import { is } from 'immer/dist/internal';
 
 function NewTicket() {
   const {user} = useSelector((state) => state.auth);
+  const { isLoading, isError, isSuccess, message} = useSelector((state) => state.ticket);
+
   const [name] = useState(user.name);
   const [email] = useState(user.email);
   const [product, setProduct] = useState('iPhone');
   const [description, setDescription] = useState('');
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    if(isSuccess) {
+      dispatch(reset())
+      navigate('/tickets')       
+    }
+
+    dispatch(reset())
+  }, [dispatch, isError, isSuccess, navigate, message])
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    dispatch(createTicket({ product, description }))
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
   return (
