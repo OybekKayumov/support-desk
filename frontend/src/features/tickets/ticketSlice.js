999523ee71e397fd7e6f1d9ef6c805d6945ddba8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-24 05:44:02 +0500

    Add create neeww ticket

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,6 +10,24 @@ const initialState = {
   message: ''
 }
 
+// Create new ticket
+export const createTicket = createAsyncThunk('tickets/create', async (ticketData, thunkAPI) => {
+  // console.log('user: ', user);
+  try {
+    const token = thunkAPI.getState().auth.user.token;
+    return await ticketService.createTicket(ticketData, token)
+  } catch (error) {
+    const message = 
+      (error.response && 
+        error.response.data && 
+        error.response.data.message) || 
+        error.message || 
+        error.toString()
+        
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
