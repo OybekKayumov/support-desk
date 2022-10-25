ad90a9a2125c6877f69d099930bcc70a244b85d9
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-26 04:45:55 +0500

    Update page Ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {toast} from 'react-toastify'
+import {getTicket, reset} from '../features/tickets/ticketSlice.js'
+import {BackButton} from '../components/BackButton'
+import {Spinner} from '../components/Spinner'
+ 
 function Ticket() {
+  const {ticket, isLoading, isSuccess, isError, message} = useSelector((state) => state.tickets)
+
+  const params = useParams()
+  const dispatch = useDispatch()
+  const {ticketId} = useParams()
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+    
+    dispatch(getTicket(ticketId))
+  }, [isError, message, ticketId])
+  
+
   return (
     <div>Ticket</div>
   )
