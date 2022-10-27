e2dd9bed554931d1bc22fc1f8a9c892d0061759c
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-27 06:36:57 +0500

    Add frontend\features: noteSlice

---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import noteService from './noteService';
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: ''
+}
+
+export const noteSlice = createSlice({
+  name: 'note',
+  initialState,
+  reducers: {
+    reset: (state) => initialState
+  },
+  extraReducers: (builder) => {
+
+  }
+})
+
+export const { reset } = noteSlice.actions
+export default noteSlice.reducer
