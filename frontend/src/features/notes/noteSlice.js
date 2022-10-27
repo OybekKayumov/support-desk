61a5abe54a55d0e6d49dc61eea42d70c95fd3f06
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-27 06:45:02 +0500

    Add frontend\features: noteService getNotes

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -9,6 +9,26 @@ const initialState = {
   message: ''
 }
 
+// Get ticket notes
+export const getNotes = createAsyncThunk(
+  'notes/getAll', 
+  async (ticketId, thunkAPI) => {
+  
+  try {
+    const token = thunkAPI.getState().auth.user.token
+    return await noteService.getNotes(ticketId, token)
+  } catch (error) {
+    const message = 
+      (error.response && 
+        error.response.data && 
+        error.response.data.message) || 
+        error.message || 
+        error.toString()
+        
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const noteSlice = createSlice({
   name: 'note',
   initialState,
